4cb5e09793a48d3b48016046c2628734877ad5fe
valdisaglonietis <valdis.aglonetis@gmail.com>
2024-11-09 18:06:36 +0200

    added idea similarity endpoint functionality

--- Dockerfile
+++ Dockerfile
@@ -8,9 +8,20 @@ WORKDIR /app
 
 # Install system dependencies for pyodbc
 RUN apt-get update && apt-get install -y \
+    build-essential \
+    curl \
+    apt-transport-https\
+    gnupg2 \
     unixodbc \
     unixodbc-dev \
+    apt-utils \
     && rm -rf /var/lib/apt/lists/*
+
+RUN curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+RUN curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
+RUN apt-get update
+RUN env ACCEPT_EULA=Y apt-get install -y msodbcsql18
 
 # Install Python dependencies
 COPY requirements.txt .
--- app/main.py
+++ app/main.py
@@ -4,8 +4,11 @@ from pydantic import BaseModel
 from typing import List
 import pyodbc
 from sentence_transformers import SentenceTransformer, util
+import torch
 import numpy as np
 from fastapi.middleware.cors import CORSMiddleware
+import os
+
 
 app = FastAPI()
 
@@ -20,75 +23,87 @@ app.add_middleware(
 
 # Load the model for embedding
 # model = SentenceTransformer('all-MiniLM-L6-v2')
+model = SentenceTransformer('models/all-MiniLM-L6-v2')
 
 # Database connection string (update with your actual values)
-# DB_CONNECTION_STRING = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=db;DATABASE=YourDatabaseName;UID=sa;PWD=YourPassword"
+db_host = os.getenv("DB_HOST", "10.87.3.162")
+db_port = os.getenv("DB_PORT", "1455")
+db_database = os.getenv("DB_DATABASE", "Ideas")
+db_user = os.getenv("DB_USER", "sa")
+db_password = os.getenv("DB_PASSWORD", "Admin@123")
+db_driver = os.getenv("DB_DRIVER", "{ODBC Driver 18 for SQL Server}")
 
-# In-memory storage for ticket embeddings
+connection_string = (
+    f"DRIVER={{ODBC Driver 18 for SQL Server}};"
+    f"SERVER={db_host},{db_port};"
+    f"DATABASE={db_database};"
+    f"UID={db_user};"
+    f"PWD={db_password};"
+    f"TrustServerCertificate=yes"
+)
+
+# In-memory storage for idea embeddings
 idea_data = {
-    "texts": [],          # List of ticket texts
-    "embeddings": []      # List of embeddings corresponding to each text
+    "rows": [],
 }
 
+# Function to load ideas from the database and store embeddings in memory
+def load_ideas():
+    print("Loading Ideas")
+
+    global idea_data
+    with pyodbc.connect(connection_string) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT \
+            id, \
+            description, \
+            (SELECT IdeaStatuses.Name FROM IdeaStatuses WHERE IdeaStatuses.Id = Ideas.IdeaStatusId) AS status, \
+            subject, \
+            CreatedOn as created_at \
+            FROM Ideas \
+        ")
+
+        # Fetch all ideas and compute embeddings once
+        idea_data["rows"] = [{
+            "id": row[0],
+            "description": row[1],
+            "status": row[2],
+            "subject": row[3],
+            "created_at": row[4],
+        } for row in cursor.fetchall()]
+        idea_data["descriptions"] = [row["description"] for row in idea_data["rows"]]
+        idea_data["unnormalized_embeddings"] = model.encode(idea_data["descriptions"], convert_to_tensor=True)
+        # Normalize embeddings to unit vectors for cosine similarity
+        idea_data["embeddings"] = torch.nn.functional.normalize(idea_data["unnormalized_embeddings"], p=2, dim=1)
+    print("Finished loading ideas")
+
+load_ideas()
+
 # Data model for incoming similarity request
-class TicketRequest(BaseModel):
+class IdeaRequest(BaseModel):
     description: str
 
-# Function to load tickets from the database and store embeddings in memory
-def load_tickets():
-    global idea_data
-
-#     with pyodbc.connect(DB_CONNECTION_STRING) as conn:
-#         cursor = conn.cursor()
-#         cursor.execute("SELECT ticket_id, ticket_text FROM tickets")
-#
-#         # Fetch all tickets and compute embeddings once
-#         idea_data["texts"] = [row[1] for row in cursor.fetchall()]
-#         idea_data["embeddings"] = model.encode(idea_data["texts"], convert_to_tensor=True)
-
-# Startup event to initialize ticket embeddings in memory
-@app.on_event("startup")
-async def startup_event():
-    load_tickets()
-
-# Similarity endpoint to find the most similar ticket
+# Similarity endpoint to find the most similar idea
 @app.post("/ideas/similarity")
-async def get_ideas_similarity(request: TicketRequest):
+async def get_ideas_similarity(request: IdeaRequest):
     # Embed the input text
-#     input_embedding = model.encode(request.text, convert_to_tensor=True)
-#
-#     # Calculate similarity scores with preloaded embeddings
-#     similarities = util.cos_sim(input_embedding, idea_data["embeddings"]).squeeze()
-#
-#     # Find the ticket with the highest similarity
-#     best_match_idx = np.argmax(similarities)
-#     best_match_score = similarities[best_match_idx].item()
+    unnormalized_input_embedding = model.encode(request.description, convert_to_tensor=True)
+    input_embedding = torch.nn.functional.normalize(unnormalized_input_embedding, p=2, dim=0)
+
+    similarities = util.cos_sim(input_embedding, idea_data["embeddings"]).squeeze()
+    sorted_indices = torch.argsort(similarities, descending=True).tolist()
+
+    top_3_suggestions = [
+        {
+            "id": idea_data["rows"][idx]["id"],
+            "status": idea_data["rows"][idx]["status"],
+            "subject": idea_data["rows"][idx]["subject"],
+            "description": idea_data["rows"][idx]["description"],
+            "created_at": idea_data["rows"][idx]["created_at"],
+            "similarity_score": int(similarities[idx].item() * 100)
+        } for idx in sorted_indices[:3]
+    ]
 
     return {
-        "data": [
-            {
-                "id": "11",
-                "status": "in production",
-                "subject": "Transferring contracts",
-                "description": "There are problems for transferring contracts from one supplier, we need to reduce time it takes for one supplier to request information",
-                "similarity_score": 99,
-                "created_at": 1731155600
-            },
-            {
-                "id": "22",
-                "status": "in development",
-                "subject": "Introducing titan alloys in towers",
-                "description": "There are new cool titan allows that could be used for building electricity towers, we should use them. They are expensive, but their longevity and maintenance is neglible",
-                "similarity_score": 85,
-                "created_at": 1699619600
-            },
-            {
-                "id": "33",
-                "status": "rejected",
-                "subject": "Introduing network overlays under rivers",
-                "description": "It would be cool to lay down network cables down the rivers. Most people live near rivers anyway. It would be cool to lay them down as it should be easy to put them in. It will also be easy to maintain unless it is winter. Now that I think about it it might be a bad idea, but please still consider it. Maybe we have a chance moving forward with this.",
-                "similarity_score": 79,
-                "created_at": 1573475600
-            }
-        ]
+        "data": top_3_suggestions,
     }
